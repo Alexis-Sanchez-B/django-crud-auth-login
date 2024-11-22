bcc0ba6346447bace32b719811c2bbc157e112d0
Alexis-Sanchez-B <sanchezalexis1994@gmail.com>
2024-11-21 22:00:18 -0300

    Agregados diseños a formularios y muestras de resultados

--- tasks/forms.py
+++ tasks/forms.py
@@ -1,8 +1,14 @@
-from django.forms import ModelForm
+#from django import ModelForm
 from .models import Task
+from django import forms
 
 #Crea Formulario en base a la clase Task creada en la BD solo con los datos necesarios
-class TaskForm(ModelForm):
+class TaskForm(forms.ModelForm):
     class Meta:
         model = Task
         fields = ['title','description','important']
+        widgets = {
+            'title': forms.TextInput(attrs={'class':'form-control','placeholder':'Write a Title'}),
+            'description': forms.Textarea(attrs={'class':'form-control','placeholder':'Write a Description'}),
+            'important': forms.CheckboxInput(attrs={'class':'form-check-input'})
+        }
